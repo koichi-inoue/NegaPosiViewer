be71126fc5cd203dd14b59e48a16b74ac13a7151
koichi-inoue <inoue.ko@gmail.com>
2023-08-29 17:44:08 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-async function setupCamera() {
+async function startCapture(){
   const stream = await navigator.mediaDevices.getUserMedia({'audio': false, 'video': true});
 	const video = document.getElementById('webcam');
   video.srcObject = stream;
-}
-
-async function startCapture(){
-  await setupCamera();
-  const deviceInfos = await navigator.mediaDevices.enumerateDevices();
-	deviceInfos.forEach(deviceInfo=>{
-		console.log(deviceInfo.kind, deviceInfo.label, deviceInfo.deviceId);
-	})
-	const constraints = await navigator.mediaDevices.getSupportedConstraints();
-	for (const [key, value] of Object.entries(constraints)) {
-    console.log(`${key}: ${value}`);
-  }
-  navigator.mediaDevices.ondevicechange = function(event) {
-    console.log("ondevicechange", event)
-  }
 }
 
 window.onload = function(){
